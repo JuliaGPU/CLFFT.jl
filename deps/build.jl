360244e0b806e7a48a6927fe3f107baead9b1821
Mus M <mus-m@outlook.com>
2017-04-10 16:24:38 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,20 @@
 using BinDeps
+using Compat
+
 @BinDeps.setup
 libnames = ["libCLFFT", "clFFT", "libclFFT"]
 libCLFFT = library_dependency("libCLFFT", aliases = libnames)
 baseurl = "https://github.com/clMathLibraries/clFFT/releases/download/v2.12.2/clFFT-2.12.2-"
+
 # download a pre-compiled binary (built by GLFW)
 if is_windows()
     if Sys.ARCH == :x86_64
-        archive = "clFFT-2.12.2-" * "Windows-x64"
-        libpath = joinpath(archive, "bin")
         uri = URI(baseurl * "Windows-x64.zip")
+        basedir = joinpath(@__DIR__, "clFFT-2.12.2-Windows-x64")
         provides(
             Binaries, uri,
-            libCLFFT, unpacked_dir = archive ,
-            installed_libpath = libpath, os = :Windows
+            libCLFFT, unpacked_dir = basedir,
+            installed_libpath = joinpath(basedir, "bin"), os = :Windows
         )
     else
         error("Only 64 bits windows supported with automatic build")
@@ -22,13 +24,12 @@ end
 if is_linux()
     provides(AptGet, "libclfft-dev", libCLFFT)
     if Sys.ARCH == :x86_64
-        archive = "clFFT-2.12.2-" * "Linux-x64"
-        libpath = joinpath(archive, "bin")
         uri = URI(baseurl * "Linux-x64.tar.gz")
+        basedir = joinpath(@__DIR__, "clFFT-2.12.2-Linux-x64")
         provides(
             Binaries, uri,
-            libCLFFT, unpacked_dir = archive,
-            installed_libpath = libpath, os = :Linux
+            libCLFFT, unpacked_dir = basedir,
+            installed_libpath = joinpath(basedir, "bin"), os = :Linux
         )
     end
 end
