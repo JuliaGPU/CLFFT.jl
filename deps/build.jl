2baf1272a2faf07c858a263ff1dba6aee5c2c332
Mus M <mus-m@outlook.com>
2017-04-10 23:07:15 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,13 +4,13 @@ using Compat
 @BinDeps.setup
 libnames = ["libCLFFT", "clFFT", "libclFFT"]
 libCLFFT = library_dependency("libCLFFT", aliases = libnames)
-baseurl = "https://github.com/clMathLibraries/clFFT/releases/download/v2.12.2/clFFT-2.12.2-"
-
+version = "2.12.2"
+baseurl = "https://github.com/clMathLibraries/clFFT/releases/download/v$(version)/clFFT-$(version)-"
 # download a pre-compiled binary (built by GLFW)
 if is_windows()
     if Sys.ARCH == :x86_64
         uri = URI(baseurl * "Windows-x64.zip")
-        basedir = joinpath(@__DIR__, "clFFT-2.12.2-Windows-x64")
+        basedir = joinpath(@__DIR__, "$(version)-Windows-x64")
         provides(
             Binaries, uri,
             libCLFFT, unpacked_dir = basedir,
@@ -25,7 +25,7 @@ if is_linux()
     provides(AptGet, "libclfft-dev", libCLFFT)
     if Sys.ARCH == :x86_64
         uri = URI(baseurl * "Linux-x64.tar.gz")
-        basedir = joinpath(@__DIR__, "clFFT-2.12.2-Linux-x64")
+        basedir = joinpath(@__DIR__, "clFFT-$(version)-Linux-x64")
         provides(
             Binaries, uri,
             libCLFFT, unpacked_dir = basedir,
